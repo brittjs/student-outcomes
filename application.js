c514fd8f18227db6b3f724ecc6a7f9d48543f8ea
Brittany See <brittanyjsee@gmail.com>
2016-02-08 05:21:00 -0800

    scroll reveal

---
+++ application.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+
+window.sr = ScrollReveal({duration: 900, viewFactor: 0.5}).reveal('#img547, #white300 img, #ourStudents img, #theyAreBubble, #map, #cards, #demoDay, #over116, #alumni33 img, #over50 img, .thankYou img');
+
+});
